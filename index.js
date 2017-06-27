04a04529cb3fe2c0042e54391999ca98611c028b
André Ferreira <contato@andreferreira.me>
2017-06-27 11:49:11 -0300

    Add basic funcionality

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+
+  io.emit('user connected', 'Anônimo conectado');
+  console.log('a user connected');
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg)
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
